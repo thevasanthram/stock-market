fced72b809f39da682c9b58371e3660684e76fb0
Vasanth Ram <82960398+thevasanthram@users.noreply.github.com>
2022-05-24 16:57:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -453,6 +453,6 @@ app.get('/logout' , (req, res)=>{
     res.redirect('/login')
 })
 
-app.listen(3000, ()=>{
-    console.log("server listening on the port 3000")
+app.listen(80, ()=>{
+    console.log("server listening on the port 80")
 });
